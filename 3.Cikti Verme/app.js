e26631e91a938ca08c3a3d44a41eaa081aeb5ddc
beytullahatasoy <beytullahatasoy55@gmail.com>
2025-12-07 01:37:53 +0300

    JavaScript learning phase

---
+++ 3.Cikti Verme/app.js
@@ -0,0 +1,34 @@
+// Document write ile çıktı verme
+document.writeln("Hello World <br>"); 
+document.writeln("Beytullah <br>");
+document.writeln("55 <br>"); 
+
+// console log metodunu kullanırız
+console.log("Hello World");
+console.log(3.53);
+
+console.clear(); // temizleme
+
+// Alert (uyarı) popup ile çıktı verme işlemleri
+let a=6 ;
+let b=8;
+
+alert("İki sayinin toplami: " + (a+b));
+
+
+// Window ve Document Nesnesi:
+console.log(window); // consele da bakabilirsin tüm obje ve metotlara
+console.log(location.port);
+
+// window > document > createElement
+console.log(document.createElement);
+document.createElement("div");
+
+
+/*
+    Yorum Satırı
+    ? Yorum Satırı
+    ! Önemli
+    TODO görev 1
+    *Test mesaj.. 
+*/
