4daaf8c5dad544133f59dd75768c742185a2192d
beytullahatasoy <beytullahatasoy55@gmail.com>
2025-12-07 12:14:07 +0300

    Veri Tipleri

---
+++ 5.Veri tipleri/app.js
@@ -0,0 +1,74 @@
+/*
+? Veri Tipleri
+
+1-String
+2-Number
+3-Boolean
+4-Null
+5-Undefined
+6-Object
+7-Function
+*/
+
+//----------------STRİNG VERİTİPİ-----------------
+// let isim = "Beytullah";
+// console.log(isim);
+// console.log(typeof isim);
+
+//----------------NUMBER VERİTİPİ-----------------
+
+// 5 +2 = 7 
+
+// let a = "5";
+// let b = "2"; çıktı:52
+
+// let stringDegisken  ="Yaşınız : ";
+// let yas = 23;
+
+// console.log(stringDegisken + yas);
+
+//----------------BOOLEAN VERİTİPİ-----------------
+
+//boolean : true ve false 'dan oluşur
+
+// console.log(5>8);
+
+// let a = 3;
+// let b = 12;
+
+// let sonuc = a+b;
+// console.log(sonuc!=15);
+// console.log(sonuc==15);
+
+//----------------NULL VERİTİPİ-----------------
+
+// let a = null; // boş
+// a=27;
+// console.log(a);
+
+//----------------UNDEFİNED VERİTİPİ-----------------
+
+// let a;
+// console.log(typeof a);   // tanımlanmamış yani değer ataması yok
+
+// primitive(ilkel) ve referans(modern) tipler 
+
+//----------------OBJECT VERİTİPİ-----------------
+// let insan = {
+//     isim : "Beytu",
+//     soyisim : "Atasoy",  // OBJECT
+//     yas : 21 
+// }
+
+// console.log(typeof insan);
+
+
+// let rakamlar =[1,2,3,4,5];
+// console.log(typeof rakamlar);    // OBJECT
+
+// let func = function(){
+//     console.log("Merhaba");  // function
+// }
+
+// func();
+// console.log(typeof func);    // function
