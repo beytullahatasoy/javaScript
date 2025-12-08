a03b62fe56cdd5bd70cf450cd1fa3c452e8a2956
beytullahatasoy <beytullahatasoy55@gmail.com>
2025-12-08 22:14:01 +0300

    VKI Hesaplama

---
+++ 8.Vucut Kitle Endeksi Hesaplama/app.js
@@ -0,0 +1,14 @@
+let kilo = Number(prompt("Kilonuzu giriniz: "));
+let boy = Number(prompt("Boyunuzu metre cinsinden giriniz: "));
+
+let sonuc = kilo/(boy**2);
+
+if(sonuc<18.5){
+    console.log("İdeal kilonun altında" + sonuc);
+}else if(sonuc>= 18.5 && sonuc<= 24.9){
+    console.log("İdeal kiloda" + sonuc);
+}else if(sonuc>= 25 && sonuc<= 29.9){
+    console.log("İdeal kilonun üstünde" + sonuc);
+}else if(sonuc>=40){
+    console.log("İdeal kilonun cok üstünde" + sonuc);
+}
