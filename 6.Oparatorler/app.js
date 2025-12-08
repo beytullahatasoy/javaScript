2236a30f7d7294388f117b1dd059030741d47a40
beytullahatasoy <beytullahatasoy55@gmail.com>
2025-12-08 19:07:24 +0300

    Oparetorler

---
+++ 6.Oparatorler/app.js
@@ -0,0 +1,43 @@
+// ARİTMATİK OPARETORLER
+
+let not1= 60;
+let not2= 60;
+
+console.log(not1+not2);
+console.log("Notlarizin toplami: " + (not1+not2));
+
+//  +, -, /, % bildiğimiz matematik işlemi
+//  ++ : 1 arttırmak için kullanılır
+//  -- : 1 azaltmak için kullanılır 
+//  ** : üssünü almak için kullanılır (örn: 5**3)
+
+// ATAMA OPARETORLERI
+/*
+    =       atama yapma
+    ==      eşit mi
+    ===     hem tipleri hemde değerleri eşit mi
+    +=      +=2 sayıyı 2 arttırır kolayca
+    -=      +=2 sayıyı 2 azaltır kolayca
+    *=      *=5 sayıyı 5 ile çarpar 
+    /=      /=2 sayıyı 2ye böler
+    %=      %=2 sayının 2ye göre modunu al
+    **=     **=2 sayının 2 üssünü al
+*/
+
+// Mantıksal Operatorler
+/*
+    && : ve  
+    || : veya
+    !  : değil
+*/
+
+// Karsilastirma Operatorleri
+/*
+    == : eşit mi
+    != : eşit değil mi
+    > : büyük mü
+    < : kücük mü
+
+    >= büyük eşit mi
+    <= küçük eşit mi
+*/
